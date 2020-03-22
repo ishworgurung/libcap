d6dbbf3f5a1801f056d7ca9d01822ca45f136d3c
Ishwor Gurung <me@ishworgurung.com>
2020-03-22 14:32:56 +1100

    test libcap binding for Go

---
+++ cap/syscalls.go
@@ -0,0 +1,27 @@
+// +build linux
+
+package cap
+
+import (
+	"github.com/ishworgurung/libcap/psx"
+	"syscall"
+)
+
+// multisc provides syscalls overridable for testing purposes that
+// support a single kernel security state for all OS threads.
+// (Go build tree has no syscall.PerOSThreadSyscall support.)
+var multisc = &syscaller{
+	w3: psx.Syscall3,
+	w6: psx.Syscall6,
+	r3: syscall.RawSyscall,
+	r6: syscall.RawSyscall6,
+}
+
+// singlesc provides a single threaded implementation. Users should
+// take care to ensure the thread is OS locked.
+var singlesc = &syscaller{
+	w3: syscall.RawSyscall,
+	w6: syscall.RawSyscall6,
+	r3: syscall.RawSyscall,
+	r6: syscall.RawSyscall6,
+}
